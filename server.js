a1a7b3ab8a84dcf1c07ae4fa735bb09dfea7afa4
omarakamal <omaraakamal@gmail.com>
2026-04-06 18:23:37 +0300

    update

---
+++ server.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const logger = require('morgan');
+const authRouter = require('./controllers/auth.routes');
+const verifyToken = require('./middleware/verify-token');
+
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(logger('dev'));
+
+// Routes go here
+app.use('/auth', authRouter);
+
+app.get('/omar',verifyToken,(req,res)=>{
+  res.json("SUCCESS")
+})
+
+
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
