27a4437a06a107d99761f02530d76e2300ba8b60
omarakamal <omaraakamal@gmail.com>
2026-04-06 19:01:29 +0300

    update

--- controllers/hoots.routes.js
+++ controllers/hoots.routes.js
@@ -1,7 +1,12 @@
 const router = require('express').Router()
+const Hoot = require('../models/Hoot')
+const verifyToken = require('../middleware/verify-token')
 
-
-
+// To create a new hoot we HAVE to be signed in
+router.post('/', verifyToken ,async (req,res)=>{
+    console.log(req.user._id)
+    res.json("CREATE ROUTE")
+})
 
 
 module.exports = router
